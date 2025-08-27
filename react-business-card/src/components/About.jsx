515b7f5a3169aa3132b57fb5d46608e63a5cf28f
Coding <codingmaniac227@gmail.com>
2025-08-27 04:41:55 -0400

    chore: added header to About section

--- react-business-card/src/components/About.jsx
+++ react-business-card/src/components/About.jsx
@@ -1,5 +1,16 @@
 export default function About() {
     return (
-        <img src="./assets/headshot.jpg"/>
+        <>
+            <img src="./assets/headshot.jpg"/>
+            <header className="header">
+                <h1>Marquise Davis</h1>
+                <h2>Fullstack Developer</h2>
+                <h3>North Carolina</h3>
+                <nav>
+                    <a></a>
+                    <a></a>
+                </nav>
+            </header>
+        </>
     )
 }
